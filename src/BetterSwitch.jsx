b9f43f9d1928cdbae805e54f8a74b6c75129ef19
Jonathan <Jonathanaguayoalpha@gmail.com>
2021-07-08 12:40:42 -0700

    heroku

--- src/BetterSwitch.jsx
+++ src/BetterSwitch.jsx
@@ -3,5 +3,9 @@ import {Switch} from 'react-router-dom';
 
 export default function Login (props)
 {
-
+    return(
+        <form method = 'post' action='/auth/google'>
+            
+        </form>
+    )
 }
